cbc3a68d8be4d384548ca6261c7abc3e33f27364
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-07-11 16:06:11 -0700

    debug

--- main.h
+++ main.h
@@ -1,11 +1,6 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-
-
 void _sprintf(const char *str);
 int _printf(const char *format, ...);
 void _count(int *ptr, char *str);
--- printf.c
+++ printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
-
+#include <stdarg.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...)
 {
