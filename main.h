f33769c146b74d52969a69b6ea2d6208642aa0d3
Joshua Ibitoye <joshuaibitoye111@gmail.com>
2022-07-12 23:13:12 -0700

    changes

--- main.h
+++ main.h
@@ -8,4 +8,5 @@ char * myitoa(int num);
 int _tobinary(int num);
 char *myitoa(int num);
 char *rev_string(char *s);
+char _putchar(char c);
 #endif
--- strout.c
+++ strout.c
@@ -1,33 +1,12 @@
-#include <stdlib.h>
-#include <unistd.h>
 
-/**
- * _sprintf - Printf function
- * @str: string
- * Return: Printed chars.
- */
+#include "main.h"
 
 void _sprintf(const char *str)
 {
-	int i, len = 0;
-	char *newstr = NULL;
-
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	newstr = malloc(sizeof(char) * (len + 1));
-
-	if (newstr == NULL)
-		exit (1);
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		newstr[i] = str[i];
+		_putchar(str[i]);
 	}
-	newstr[i] = '\0';
-
-	write(1, newstr, len);
-	free(newstr);
 }
